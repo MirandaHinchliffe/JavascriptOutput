bb96a0d08b91387ba1130f0143db22c8a91b0dd1
Miranda Hinchliffe <70153183+MirandaHinchliffe@users.noreply.github.com>
2020-12-09 13:38:56 -0500

    Add files via upload

---
+++ JSoutput.js
@@ -0,0 +1,14 @@
+// Enter your code below to edit index.html
+
+yourName = "Ninjafiveo"
+document.getElementById("yourName").innerHTML = "Miranda Hinchliffe";
+document.getElementById("myBio").innerHTML = "I am a very quiet person, and I can also be a very outgoing, smiley person too. Depends on my day. I love art and I'd say I somewhat of a very creative person "
+document.getElementById("collegeChoice").innerHTML ="I am not entirely sure if I am going to go to college. If I do, I will more than likely join the Navy before, but if I had to choose a college I would do it online, and possibly choose Rasmussen, or some online college like that."
+document.getElementById("programChoice").innerHTML ="It offers Software Aplication Development and Web Programming. so i would go for one of those, and I would get my Associate's Degree from it."
+document.getElementById("collegeReason").innerHTML ="The reason for me chosing this college is because it's online and I would rather avoid people than be around a bunch of them in a lecture for hours. "
+document.getElementById("careerChoice").innerHTML ="My career choice, I am not entirely sure of yet so I would say I am indecisive"
+document.getElementById("companyChoice").innerHTML ="N/A I'm jobless 😖"
+document.getElementById("careerReason").innerHTML ="N/A🚫"
+document.getElementById("answerOne").innerHTML ="Document Object Model, AKA DOM, is an application program that is used for HTML and XML documents. It represents a 'logical tree'. It also defines  the way a document is accessed and manipulated "   
+document.getElementById("answerTwo").innerHTML = " Different ways for writing Javescript outputs can look like, HTML = innerHTML. HTML output = document.write(). Alert box = window.alert(). Browser consle = console.log()."
+document.getElementById("answerThree").innerHTML ="Javascript basically 'has all the power'.Java can change all of the HTML elements and attributes in the page. It can also change all the CSS too."
